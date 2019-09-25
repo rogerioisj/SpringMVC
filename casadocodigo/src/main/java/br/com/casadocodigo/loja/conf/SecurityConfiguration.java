2a2e8ba911038c104f8303b9b3132de59098f6f4
Roger <rogerio.reaper@gmail.com>
2019-09-24 21:27:48 -0300

    Atualização

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,23 +14,40 @@ import br.com.casadocodigo.loja.DAO.UsuarioDAO;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	
 	@Autowired
 	private UsuarioDAO usuarioDao;
-
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/resources/**").permitAll().antMatchers("/carrinho/**").permitAll()
-				.antMatchers("/pagamento/**").permitAll().antMatchers("/produtos/form").hasRole("ADMIN")
-				.antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN").antMatchers(HttpMethod.GET, "/produtos")
-				.hasRole("ADMIN").antMatchers("/produtos/**").permitAll().antMatchers("/").permitAll().anyRequest()
-				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		http.authorizeRequests()
+//			.antMatchers("/resources/**").permitAll()
+			.antMatchers("/produtos/form").hasRole("ADMIN")
+			.antMatchers("/produtos").hasRole("ADMIN")
+			.antMatchers("/produtos/").hasRole("ADMIN")
+			.antMatchers("/produtos/**").permitAll()
+			.antMatchers("/carrinho/**").permitAll()	
+			.antMatchers("/pagamento/**").permitAll()	
+			.antMatchers("/").permitAll()
+			.antMatchers("/url-magica-maluca-oajksfbvad6584i57j54f9684nvi658efnoewfmnvowefnoeijn").permitAll()
+			.anyRequest().authenticated()
+			.and()
+				.formLogin().loginPage("/login").defaultSuccessUrl("/produtos").permitAll()
+			.and()
+				.logout()
+	            .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll() 
+	            .logoutSuccessUrl("/login");
 	}
-
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(usuarioDao).passwordEncoder(new BCryptPasswordEncoder());
+		auth.userDetailsService(usuarioDao)
+				.passwordEncoder(new BCryptPasswordEncoder());
 	}
-
+	
+    // Forma recomendada de ignorar no filtro de segurança as requisições para recursos estáticos
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
 }
